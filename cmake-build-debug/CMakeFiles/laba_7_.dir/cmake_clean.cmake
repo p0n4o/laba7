918d29026349c12fe53de29ab9c83b4fd78660bd
p0n40 <nazar_2004@icloud.com>
2023-02-28 10:59:35 +0300

    file changes3

---
+++ cmake-build-debug/CMakeFiles/laba_7_.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba_7_.dir/main.c.o"
+  "CMakeFiles/laba_7_.dir/main.c.o.d"
+  "laba_7_"
+  "laba_7_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laba_7_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/laba_7_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for laba_7_.
+# This may be replaced when dependencies are built.
